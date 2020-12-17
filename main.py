5096e78a0b0e84c53c90696fd9536f450b486ff2
Felipe <andres.fhortua@gmail.com>
2020-12-17 14:41:30 -0500

    backend funcionando

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException
+import db
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+ "http://localhost:8080"
+]
+app.add_middleware(
+CORSMiddleware, 
+allow_origins=origins,
+allow_credentials=True, 
+allow_methods=["*"], 
+allow_headers=["*"],
+)
+
+@app.get("/documentos/")
+async def obtener_docs():
+    doc = db.obtenet_docs()
+    return doc
+
+@app.post("/documentos/nuevo/")
+async def crear_doc(doc: db.Documento):
+    ok = db.crear_docs(doc)
+    if ok:
+        return {'mensaje':'Documento creado'}
+    else:
+        raise HTTPException(status_code=400, detail="Documento existente")
