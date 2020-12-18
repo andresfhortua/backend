e22cb36ff0a44a8e059784dec441c9bbb5968b22
andresfhortua <32169787+andresfhortua@users.noreply.github.com>
2020-12-17 20:06:16 -0500

    Update db.py

--- db.py
+++ db.py
@@ -8,9 +8,9 @@ class Documento(BaseModel):
     fecha_venc: str   
 
 documentos = {
-    1: Documento(id_documento=1, nombre="cotizacion_1", id_dependencia=2, prioridad= 1, fecha_venc="10-10-2020"),
-    2: Documento(id_documento=2, nombre="cuenta_cobro_1", id_dependencia=2, prioridad= 2, fecha_venc="25-11-2020"),
-    3: Documento(id_documento=3, nombre="citacion_1", id_dependencia=1, prioridad= 3, fecha_venc="15-12-2020")
+    1: Documento(id_documento=1, nombre="cotizacion_1", id_dependencia=2, prioridad= 1, fecha_venc="2020-10-10"),
+    2: Documento(id_documento=2, nombre="cuenta_cobro_1", id_dependencia=2, prioridad= 2, fecha_venc="2020-11-25"),
+    3: Documento(id_documento=3, nombre="citacion_1", id_dependencia=1, prioridad= 3, fecha_venc="2020-12-25")
 }
 
 def obtenet_docs():
